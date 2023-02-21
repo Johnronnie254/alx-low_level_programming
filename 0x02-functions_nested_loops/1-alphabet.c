33a463f0cbdddccc1eb6fe62f61d25d4f335a991
Johnronnie254 <johnronnie18@gmail.com>
2023-02-21 14:37:58 -0800

    try again

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,17 +7,11 @@
 void print_alphabet(void)
 {
 
-	int i;
-	int c;
-
-	i=0;
-	while (i < 10)
-	{
+	char c;
 
 		for (c = 'a'; c <= 'z'; c++)
-			_putchar(c);
-
-	}
-	i++;
-	_putchar(c);
+		{
+		_putchar(c);
+		}	
+		_putchar('\');
 }
